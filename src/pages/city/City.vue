a0ed5e4c5eb39ca3860bf1580824ffabeebd838c
wangchenxu <1042090950@qq.com>
2022-05-04 23:09:45 +0800

    performance

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -5,8 +5,14 @@
 	<div>
 		<city-header></city-header>
 		<city-search></city-search>
-		<city-list :cities="cities" :hot="hotCities"></city-list>
-		<city-alphabet :cities="cities"></city-alphabet>
+		<city-list
+    :cities="cities"
+    :hot="hotCities"
+    :letter="letter"
+    ></city-list>
+		<city-alphabet :cities="cities"
+    @change="handleLetterChange"
+    ></city-alphabet>
 	</div>
 </template>
 <script>
@@ -26,7 +32,8 @@ export default {
   data () {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   methods: {
@@ -41,6 +48,9 @@ export default {
         this.cities = data.cities
         this.hotCities = data.hotCities
       }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
     }
   },
   mounted () {
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,7 +17,11 @@
 					</div>
 				</div>
 			</div>
-			<div class="area" v-for="(item,key) of cities" :key="key">
+			<div class="area"
+			v-for="(item,key) of cities"
+			:key="key"
+			:ref="key"
+			>
 				<div class="title border-topbottom">{{key}}</div>
 				<div class="item-list">
 					<div class="item border-bottom" v-for="innerItem of item"
@@ -34,13 +38,22 @@ export default {
 	name: 'CityList',
 	props: {
 		cities: Object,
-		hot: Array
+		hot: Array,
+		letter: String,
 	},
 	mounted () {
 		this.scroll=new Bscroll(this.$refs.wrapper)
 	},
 	updated () {
 		this.scroll.refresh()
+	},
+	watch: {
+		letter () {
+			if(this.letter){
+				const element = this.$refs[this.letter][0]
+				this.scroll.scrollToElement(element)
+			}
+		}
 	}
 }
 </script>

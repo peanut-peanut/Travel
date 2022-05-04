af481c4994c1240e83de3e16eb32b4fdbb46caa9
wangchenxu <1042090950@qq.com>
2022-05-04 17:05:34 +0800

    add city-ajax

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,24 +1,51 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable space-before-blocks */
+/* eslint-disable indent */
 <template>
 	<div>
 		<city-header></city-header>
 		<city-search></city-search>
-		<city-list></city-list>
-		<city-alphabet></city-alphabet>
+		<city-list :cities="cities" :hot="hotCities"></city-list>
+		<city-alphabet :cities="cities"></city-alphabet>
 	</div>
 </template>
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
 export default {
-	name: 'City',
-	components: {
-		CityHeader,
-		CitySearch,
-		CityList,
-		CityAlphabet
-	}
+  name: 'City',
+  components: {
+    CityHeader,
+    CitySearch,
+    CityList,
+    CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.handleCityInfoSucc)
+    },
+    handleCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
+  mounted () {
+    this.getCityInfo()
+  }
 }
 </script>
 <style lang="stylus" scoped>
